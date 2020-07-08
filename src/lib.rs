6ddd10bffdb80debad00244c8bf6eb3c2a818264
Vincent Nguyen <vngu4919@uni.sydney.edu.au>
2020-07-08 17:57:33 +1000

    Added new dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,22 @@
+use punkt::{TrainingData, SentenceTokenizer, tokenizer};
+
 pub struct Pool{
-
+    metamap_pool: Vec<Metamap>,
 }
 
-pub struct Metamap{
-    training_data: 
-}
+pub struct Metamap {
+    model_data: TrainingData,
+    segmenter: SentenceTokenizer,
+} 
 
 impl Metamap {
-    pub fn segment_sentence() {
-
+    pub fn segment_sentence(self, input: &str) -> Box<dyn tokenizer> {
+        SentenceTokenizer::<Standard>::new(&str, &self.model_data)
     }
 
-    pub fn extract_concepts(input: &str, segment_sentence: bool) {
-
-    }
+    pub fn extract_concepts(input: &str, segment_sentence: bool) { 
+        segment_sentence(&input)
+            .iter()
+            .map()
+    } 
 }

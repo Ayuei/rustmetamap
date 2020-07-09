7a95cec13bc5c2fe83eb3932700f596ba9da70ba
Ayuei <sefton_student@hotmail.com>
2020-07-09 21:44:17 +1000

    Refactored main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,4 @@
-use structopt::StructOpt;
-use std::path::PathBuf;
-
-#[derive(StructOpt, Debug)]
-pub struct Opt {
-    #[structopt(name = "Metamap Location", parse(from_os_str))]
-    pub metamap_location: PathBuf,
-}
+// use std::path::PathBuf;
 
 fn main() {
-    let opts = Opt::from_args();
 }
